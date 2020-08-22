5ce5eeb53c98ecf2e88c54ba822053976e495a1b
Eder Canada <canada.eder@gmail.com>
2020-08-22 08:43:52 -0700

    credits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,8 +19,9 @@ export default function Footer() {
       <ul className="copyright">
         <li>&copy; The Atelier </li>
         <li>
-          Credits: <a href="http://unsplash.com/">Unsplash</a> +
-          <a href="http://html5up.net">HTML5 UP</a>
+          <p> Let's make it happen</p>
+          {/* Credits: <a href="http://unsplash.com/">Unsplash</a> +
+          <a href="http://html5up.net">HTML5 UP</a> */}
         </li>
       </ul>
     </footer>
