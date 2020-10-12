a58486dd4281af9139f33f2e33a74126411901e1
Eder Canada <canada.eder@gmail.com>
2020-10-12 11:49:08 -0700

    eder 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,6 +21,7 @@ export default function Footer() {
         <li>&copy; The Atelier </li>
         <li>
           <p> Let's make it happen</p>
+          <p>  {console.log('eder')} </p>
           {/* Credits: <a href="http://unsplash.com/">Unsplash</a> +
           <a href="http://html5up.net">HTML5 UP</a> */}
         </li>
