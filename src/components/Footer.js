382bc5ab33640a0524a9d916a56ada51462715d8
Eder Canada <canada.eder@gmail.com>
2020-10-12 12:42:04 -0700

    remove edits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,7 @@
 import React from 'react';
 import config from '../../config';
 
-console.log('eder')
 
-// Assign handler to message event
-if ( window.addEventListener ) {
-  window.addEventListener('message', handleMessage, false);
-} else if ( window.attachEvent ) { // ie8
-  window.attachEvent('onmessage', handleMessage);
-}
-
-// message event handler (e is event object) 
-function handleMessage(e) {
-  // Reference to element for data display
-  var el = document.getElementById('footer');
-  // Check origin
-  if ( e.origin === 'https://www.archertravel.com/evo-supplier-archer-go-eder-draft/' ) {
-      // Retrieve data sent in postMessage
-      el.innerHTML = e.data;
-      // Send reply to source of message
-      e.source.postMessage('Message received', e.origin);
-  }
-}
 
 
 export default function Footer() {
@@ -43,7 +23,6 @@ export default function Footer() {
         <li>&copy; The Atelier </li>
         <li>
           <p> Let's make it happen</p>
-          <p>  {console.log('ederr')} </p>
           {/* Credits: <a href="http://unsplash.com/">Unsplash</a> +
           <a href="http://html5up.net">HTML5 UP</a> */}
         </li>
