199878f8fbee0c1c9fd808279b3c9281be512d26
Eder Canada <canada.eder@gmail.com>
2020-10-12 12:33:15 -0700

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,6 +10,18 @@ if ( window.addEventListener ) {
   window.attachEvent('onmessage', handleMessage);
 }
 
+// message event handler (e is event object) 
+function handleMessage(e) {
+  // Reference to element for data display
+  var el = document.getElementById('display');
+  // Check origin
+  if ( e.origin === 'https://www.archertravel.com/evo-supplier-archer-go-eder-draft/' ) {
+      // Retrieve data sent in postMessage
+      el.innerHTML = e.data;
+      // Send reply to source of message
+      e.source.postMessage('Message received', e.origin);
+  }
+}
 
 
 export default function Footer() {
